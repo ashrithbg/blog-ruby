e5b959af2f3a4957eff186b31ae18c00c6afd914
Ashrith Bangi <ashrith@Ashriths-MacBook-Pro.local>
2015-12-27 20:41:16 -0500

    Added authentication using devise gem and modified the add/delete options based on whether a user is logged in or not

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,15 @@
 class CommentsController < ApplicationController
-	def new
-	end
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(params[:comment].permit(:name,:body))
 		@comment.save
 		redirect_to @post
 	end
-	def edit 
-	end
-	def show
-	end
+
 	def destroy
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.find(params[:id])
 		@comment.destroy
 		redirect_to @post
-	end
-	def update
 	end
 end
